84b79521d958249f1ca984271d3edeba003ede1d
smirnakh <99379073+smirnakh@users.noreply.github.com>
2023-02-28 06:53:30 -0800

    added new folder and files

---
+++ String-1/helloName.js
@@ -0,0 +1,11 @@
+// Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+// Examples
+
+// helloName('Bob') → Hello Bob!
+// helloName('Alice') → Hello Alice!
+// helloName('X') → Hello X!
+
+function helloName(name) {
+  return `Hello ${name}!`;
+}
